f39d95892d1e0bf456f210ed496189bc07be3433
Roshan Pius <rpius@google.com>
2016-06-09 10:54:18 -0700

    WifiConfigStoreNew: Implement file write/read
    Implement the file write/read for config store files. The read/write
    uses the |AtomicFile| API's to perform the necessary operations.
    
    The write is split into 2 steps to help buffering non-critical writes:
    1. Serialize the data into raw bytes first.
    2. Write the raw bytes to file.
    Any non-critical write (indicated by |forceWrite| flag set to false)
    will set an alarm to buffer all writes within the interval into a single
    file write. This will also help if we want to move the actual file
    writing to a separate thread later.
    
    BUG: 29179579
    Change-Id: Ib793a3967c327e13ce88acfa912c21cfa5f25865
    TEST: Unit-tests

--- service/java/com/android/server/wifi/WifiConfigStoreNew.java
+++ service/java/com/android/server/wifi/WifiConfigStoreNew.java
@@ -19,25 +19,29 @@ package com.android.server.wifi;
 import android.app.AlarmManager;
 import android.content.Context;
 import android.os.Environment;
+import android.os.FileUtils;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.os.AtomicFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * This class provides the API's to save/load/modify network configurations from a persistent
  * store. Uses keystore for certificate/key management operations.
- * NOTE: This class should only be used from WifiConfigManager!!!
+ * NOTE: This class should only be used from WifiConfigManager and is not thread-safe!
  */
 public class WifiConfigStoreNew {
     /**
      * Alarm tag to use for starting alarms for buffering file writes.
      */
     @VisibleForTesting
-    public static final String FILE_WRITE_BUFFER_ALARM_TAG = "WriteBufferAlarm";
+    public static final String BUFFERED_WRITE_ALARM_TAG = "WriteBufferAlarm";
     /**
      * Log tag.
      */
@@ -54,7 +58,7 @@ public class WifiConfigStoreNew {
     /**
      * Time interval for buffering file writes for non-forced writes
      */
-    private static final int FILE_WRITE_BUFFER_ALARM_INTERVAL_MS = 10 * 1000;
+    private static final int BUFFERED_WRITE_ALARM_INTERVAL_MS = 10 * 1000;
     /**
      * Handler instance to post alarm timeouts to
      */
@@ -62,7 +66,11 @@ public class WifiConfigStoreNew {
     /**
      * Alarm manager instance to start buffer timeout alarms.
      */
-    private AlarmManager mAlarmManager;
+    private final AlarmManager mAlarmManager;
+    /**
+     * Clock instance to retrieve timestamps for alarms.
+     */
+    private final Clock mClock;
     /**
      * Shared config store file instance.
      */
@@ -75,6 +83,24 @@ public class WifiConfigStoreNew {
      * Verbose logging flag.
      */
     private boolean mVerboseLoggingEnabled = false;
+    /**
+     * Flag to indicate if there is a buffered write pending.
+     */
+    private boolean mBufferedWritePending = false;
+    /**
+     * Alarm listener for flushing out any buffered writes.
+     */
+    private final AlarmManager.OnAlarmListener mBufferedWriteListener =
+            new AlarmManager.OnAlarmListener() {
+                public void onAlarm() {
+                    try {
+                        writeBufferedData();
+                    } catch (IOException e) {
+                        Log.wtf(TAG, "Buffered write failed");
+                    }
+
+                }
+            };
 
     /**
      * Create a new instance of WifiConfigStore.
@@ -82,20 +108,22 @@ public class WifiConfigStoreNew {
      *
      * @param context     context to use for retrieving the alarm manager.
      * @param looper      looper instance to post alarm timeouts to.
+     * @param clock       clock instance to retrieve timestamps for alarms.
      * @param sharedStore StoreFile instance pointing to the shared store file. This should
      *                    be retrieved using {@link #createSharedFile()} method.
      * @param userStore   StoreFile instance pointing to the user specific store file. This should
      *                    be retrieved using {@link #createUserFile(int)} method.
      */
-    public WifiConfigStoreNew(Context context, Looper looper,
+    public WifiConfigStoreNew(Context context, Looper looper, Clock clock,
             StoreFile sharedStore, StoreFile userStore) {
 
         mAlarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         mEventHandler = new Handler(looper);
+        mClock = clock;
 
         // Initialize the store files.
         mSharedStore = sharedStore;
-        handleUserSwitch(userStore);
+        mUserStore = userStore;
     }
 
     /**
@@ -132,25 +160,61 @@ public class WifiConfigStoreNew {
      * API to write the provided store data to config stores.
      * The method writes the user specific configurations to user specific config store and the
      * shared configurations to shared config store.
-     * Also stores other global data like blacklists, etc.
      *
      * @param forceSync boolean to force write the config stores now. if false, the writes are
      *                  buffered and written after the configured interval.
      * @param storeData The entire data to be stored across all the config store files.
+     * @throws IOException if an error occurs.
      */
-    public void write(boolean forceSync, WifiConfigStoreData storeData) {
+    public void write(boolean forceSync, WifiConfigStoreData storeData) throws IOException {
+        // Serialize the provided data and send it to the respective stores. The actual write will
+        // be performed later depending on the |forceSync| flag .
         byte[] sharedDataBytes = storeData.createSharedRawData();
         byte[] userDataBytes = storeData.createUserRawData();
 
         mSharedStore.storeRawDataToWrite(sharedDataBytes);
         mUserStore.storeRawDataToWrite(userDataBytes);
 
+        // Every write provides a new snapshot to be persisted, so |forceSync| flag overrides any
+        // pending buffer writes.
         if (forceSync) {
-            mSharedStore.writeBufferedRawData();
-            mUserStore.writeBufferedRawData();
+            writeBufferedData();
         } else {
-            // start alarm and blah, blah.
+            startBufferedWriteAlarm();
         }
+    }
+
+    /**
+     * Helper method to start a buffered write alarm if one doesn't already exist.
+     */
+    private void startBufferedWriteAlarm() {
+        if (!mBufferedWritePending) {
+            mAlarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                    mClock.getElapsedSinceBootMillis() + BUFFERED_WRITE_ALARM_INTERVAL_MS,
+                    BUFFERED_WRITE_ALARM_TAG, mBufferedWriteListener, mEventHandler);
+            mBufferedWritePending = true;
+        }
+    }
+
+    /**
+     * Helper method to stop a buffered write alarm if one exists.
+     */
+    private void stopBufferedWriteAlarm() {
+        if (mBufferedWritePending) {
+            mAlarmManager.cancel(mBufferedWriteListener);
+            mBufferedWritePending = false;
+        }
+    }
+
+    /**
+     * Helper method to actually perform the writes to the file. This flushes out any write data
+     * being buffered in the respective stores and cancels any pending buffer write alarms.
+     * @throws IOException if an error occurs.
+     */
+    private void writeBufferedData() throws IOException {
+        stopBufferedWriteAlarm();
+        mSharedStore.writeBufferedRawData();
+        mUserStore.writeBufferedRawData();
     }
 
     /**
@@ -160,8 +224,9 @@ public class WifiConfigStoreNew {
      * Also retrieves other global data like blacklists, etc.
      *
      * @return storeData The entire data retrieved across all the config store files.
+     * @throws IOException if an error occurs.
      */
-    public WifiConfigStoreData read() {
+    public WifiConfigStoreData read() throws IOException {
         byte[] sharedDataBytes = mSharedStore.readRawData();
         byte[] userDataBytes = mUserStore.readRawData();
 
@@ -173,12 +238,11 @@ public class WifiConfigStoreNew {
      *
      * @param userStore StoreFile instance pointing to the user specific store file. This should
      *                  be retrieved using {@link #createUserFile(int)} method.
+     * @throws IOException if an error occurs.
      */
-    public void handleUserSwitch(StoreFile userStore) {
-        if (mUserStore != null) {
-            // Flush out any stored data if present before switching the user stores.
-            mUserStore.writeBufferedRawData();
-        }
+    public void handleUserSwitch(StoreFile userStore) throws IOException {
+        // Flush out any stored data if present before switching the user stores.
+        writeBufferedData();
         mUserStore = userStore;
     }
 
@@ -190,6 +254,10 @@ public class WifiConfigStoreNew {
      */
     public static class StoreFile {
         /**
+         * File permissions to lock down the file.
+         */
+        private static final int FILE_MODE = 0600;
+        /**
          * The store file to be written to.
          */
         private final AtomicFile mAtomicFile;
@@ -197,18 +265,25 @@ public class WifiConfigStoreNew {
          * This is an intermediate buffer to store the data to be written.
          */
         private byte[] mWriteData;
+        /**
+         * Store the file name for setting the file permissions/logging purposes.
+         */
+        private String mFileName;
 
         public StoreFile(File file) {
             mAtomicFile = new AtomicFile(file);
+            mFileName = mAtomicFile.getBaseFile().getAbsolutePath();
         }
 
         /**
          * Read the entire raw data from the store file and return in a byte array.
          *
          * @return raw data read from the file.
+         * @throws IOException if an error occurs. The input stream is always closed by the method
+         * even when an exception is encountered.
          */
-        public byte[] readRawData() {
-            return null;
+        public byte[] readRawData() throws IOException {
+            return mAtomicFile.readFully();
         }
 
         /**
@@ -225,11 +300,28 @@ public class WifiConfigStoreNew {
         /**
          * Write the stored raw data to the store file.
          * After the write to file, the mWriteData member is reset.
+         * @throws IOException if an error occurs. The output stream is always closed by the method
+         * even when an exception is encountered.
          */
-        public void writeBufferedRawData() {
-            if (mWriteData != null) {
-                // write data.
+        public void writeBufferedRawData() throws IOException {
+            if (mWriteData == null) {
+                Log.w(TAG, "No data stored for writing to file: " + mFileName);
+                return;
             }
+            // Write the data to the atomic file.
+            FileOutputStream out = null;
+            try {
+                out = mAtomicFile.startWrite();
+                FileUtils.setPermissions(mFileName, FILE_MODE, -1, -1);
+                out.write(mWriteData);
+                mAtomicFile.finishWrite(out);
+            } catch (IOException e) {
+                if (out != null) {
+                    mAtomicFile.failWrite(out);
+                }
+                throw e;
+            }
+            // Reset the pending write data after write.
             mWriteData = null;
         }
     }
