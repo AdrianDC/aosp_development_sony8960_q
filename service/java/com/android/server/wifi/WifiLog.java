573de1504ea684a2a647613659d4771c2a315eea
mukesh agrawal <quiche@google.com>
2016-08-05 15:10:05 -0700

    logging: add chained logging API
    Add a new logging API, which uses call-chaining to generate
    log messages. The chaining approach will reduce the cost
    of disabled log messages, as such messages won't need to
    be formatted.
    
    While there: add some missing @override annotations.
    
    BUG=30736748
    TEST=wifitests/runtests.sh
    
    Change-Id: I132fd3955426b8528208a21f4f1ecd402962ffd0

--- service/java/com/android/server/wifi/WifiLog.java
+++ service/java/com/android/server/wifi/WifiLog.java
@@ -16,34 +16,183 @@
 
 package com.android.server.wifi;
 
+import android.annotation.NonNull;
+
+/**
+ * Provides an abstraction of logging back-ends.
+ *
+ * The abstraction is designed to
+ * a) minimize the cost of disabled log messages,
+ * b) allow callers to tag message parameters as containing sensitive
+ *    information,
+ * c) avoid the use of format codes, and
+ * d) easily support additional data types.
+ *
+ * Implementations of WifiLog may or may not be thread-safe.
+ * Implementations of LogMessage are expected _not_ to be thread-safe,
+ * as LogMessage instances are not expected to be shared between threads.
+ */
 public interface WifiLog {
+    char PLACEHOLDER = '%';
+
+    // New-style API.
+    /**
+     * Allocate an error-level log message, which the caller will fill with
+     * additional parameters according to |format|. After filling the message
+     * with parameters, the caller must call flush(), to actually log the message.
+     *
+     * Error-level messages should be used when a malfunction has occurred,
+     * and the malfunction is likely to cause an externally visible problem.
+     * For example: we failed to initialize the Wifi interface.
+     *
+     * Typical usage is as follows:
+     *     WifiDevice() {
+     *         mLog = new LogcatLog("ModuleName");
+     *     }
+     *
+     *     void start() {
+     *         // ...
+     *         mLog.err("error % while starting interface %").c(errNum).c(ifaceName).flush();
+     *     }
+     *
+     *     void stop() {
+     *         // ...
+     *         mLog.err("error % while stopping interface %").c(errNum).c(ifaceName).flush();
+     *     }
+     */
+    @NonNull
+    LogMessage err(@NonNull String format);
+
+    /**
+     * Like {@link #err(String) err()}, except that a warning-level message is
+     * allocated.
+     *
+     * Warning-level messages should be used when a malfunction has occurred,
+     * but the malfunction is _unlikely_ to cause an externally visible problem
+     * on its own. For example: if we fail to start the debugging subsystem.
+     */
+    @NonNull
+    LogMessage warn(@NonNull String format);
+
+    /**
+     * Like {@link #err(String) err()}, except that a info-level message is
+     * allocated.
+     *
+     * Info-level messages should be used to report progress or status messages
+     * that help understand the program's external behavior. For example: we
+     * might log an info message before initiating a Wifi association.
+     */
+    @NonNull
+    LogMessage info(@NonNull String format);
+
+    /**
+     * Like {@link #err(String) err()}, except that a trace-level message is
+     * allocated.
+     *
+     * Trace-level messages should be used to report progress or status messages
+     * that help understand the program's internal behavior. For example:
+     * "Reached myCoolMethod()".
+     */
+    @NonNull
+    LogMessage trace(@NonNull String format);
+
+    /**
+     * Like {@link #err(String) err()}, except that a dump-level message is
+     * allocated.
+     *
+     * Dump-level messages should be used to report detailed internal state.
+     */
+    @NonNull
+    LogMessage dump(@NonNull String format);
+
+    /**
+     * Represents a single log message.
+     *
+     * Implementations are expected _not_ to be thread-safe.
+     */
+    interface LogMessage {
+        /**
+         * Replace the first available placeholder in this LogMessage's format
+         * with the specified value. Mark the value as 'raw', to inform the
+         * logging daemon that the value may contain sensitive data.
+         *
+         * @return |this|, to allow chaining of calls
+         */
+        @NonNull
+        LogMessage r(String value);
+
+        /**
+         * Like {@link #r(String) r()}, except that the value is marked
+         * as 'clean', to inform the logging daemon that the value does _not_
+         * contain sensitive data.
+         */
+        @NonNull
+        LogMessage c(String value);
+
+        /**
+         * Like {@link #c(String) c(String)}, except that the value is a long.
+         */
+        @NonNull
+        LogMessage c(long value);
+
+        /**
+         * Like {@link #c(String) c(String)}, except that the value is a char.
+         */
+        @NonNull
+        LogMessage c(char value);
+
+        /**
+         * Write this LogMessage to the logging daemon. Writing the
+         * message is best effort. More specifically:
+         * 1) The operation is non-blocking. If we’re unable to write
+         *    the log message to the IPC channel, the message is
+         *    dropped silently.
+         * 2) If the number of |value|s provided exceeds the number of
+         *    placeholders in the |format|, then extraneous |value|s
+         *    are silently dropped.
+         * 3) If the number of placeholders in the |format| exceeds
+         *    the number of |value|s provided, the message is sent to
+         *    the logging daemon without generating an Exception.
+         * 4) If the total message length exceeds the logging
+         *    protocol’s maximum message length, the message is
+         *    silently truncated.
+         */
+        void flush();
+    }
+
+    // Legacy API.
     /**
      * Log an error using the default tag for this WifiLog instance.
      * @param msg the message to be logged
+     * TODO(b/30736737): Remove this method, once all code has migrated to alternatives.
      */
     void e(String msg);
 
     /**
      * Log a warning using the default tag for this WifiLog instance.
      * @param msg the message to be logged
+     * TODO(b/30736737): Remove this method, once all code has migrated to alternatives.
      */
     void w(String msg);
 
     /**
      * Log an informational message using the default tag for this WifiLog instance.
      * @param msg the message to be logged
+     * TODO(b/30736737): Remove this method, once all code has migrated to alternatives.
      */
     void i(String msg);
 
     /**
      * Log a debug message using the default tag for this WifiLog instance.
      * @param msg the message to be logged
+     * TODO(b/30736737): Remove this method, once all code has migrated to alternatives.
      */
     void d(String msg);
 
     /**
      * Log a verbose message using the default tag for this WifiLog instance.
      * @param msg the message to be logged
+     * TODO(b/30736737): Remove this method, once all code has migrated to alternatives.
      */
     void v(String msg);
 }
