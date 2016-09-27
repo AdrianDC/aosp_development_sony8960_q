2fb3a0a5f4f5c50f52bd187579102e627ff5646d
Etan Cohen <etancohen@google.com>
2016-09-27 09:53:33 -0700

    [NAN] Rename setTerminateNotificationEnabled per API review [DO NOT MERGE]
    Bug: 31470256
    Test: unit tests
    Change-Id: I1bf7b801588dfc12d47efdb90aef1d1fc2ef7b7d

--- tests/wifitests/src/com/android/server/wifi/nan/WifiNanManagerTest.java
+++ tests/wifitests/src/com/android/server/wifi/nan/WifiNanManagerTest.java
@@ -673,7 +673,7 @@ public class WifiNanManagerTest {
                 .setServiceSpecificInfo(serviceSpecificInfo).setMatchFilter(matchFilter)
                 .setSubscribeType(subscribeType)
                 .setSubscribeCount(subscribeCount).setTtlSec(subscribeTtl).setMatchStyle(matchStyle)
-                .setEnableTerminateNotification(enableTerminateNotification).build();
+                .setTerminateNotificationEnabled(enableTerminateNotification).build();
 
         collector.checkThat("mServiceName", serviceName.getBytes(),
                 equalTo(subscribeConfig.mServiceName));
@@ -706,7 +706,7 @@ public class WifiNanManagerTest {
                 .setServiceSpecificInfo(serviceSpecificInfo).setMatchFilter(matchFilter)
                 .setSubscribeType(subscribeType)
                 .setSubscribeCount(subscribeCount).setTtlSec(subscribeTtl).setMatchStyle(matchStyle)
-                .setEnableTerminateNotification(enableTerminateNotification).build();
+                .setTerminateNotificationEnabled(enableTerminateNotification).build();
 
         Parcel parcelW = Parcel.obtain();
         subscribeConfig.writeToParcel(parcelW, 0);
@@ -776,7 +776,7 @@ public class WifiNanManagerTest {
                 .setServiceSpecificInfo(serviceSpecificInfo).setMatchFilter(matchFilter)
                 .setPublishType(publishType)
                 .setPublishCount(publishCount).setTtlSec(publishTtl)
-                .setEnableTerminateNotification(enableTerminateNotification).build();
+                .setTerminateNotificationEnabled(enableTerminateNotification).build();
 
         collector.checkThat("mServiceName", serviceName.getBytes(),
                 equalTo(publishConfig.mServiceName));
@@ -805,7 +805,7 @@ public class WifiNanManagerTest {
                 .setServiceSpecificInfo(serviceSpecificInfo).setMatchFilter(matchFilter)
                 .setPublishType(publishType)
                 .setPublishCount(publishCount).setTtlSec(publishTtl)
-                .setEnableTerminateNotification(enableTerminateNotification).build();
+                .setTerminateNotificationEnabled(enableTerminateNotification).build();
 
         Parcel parcelW = Parcel.obtain();
         publishConfig.writeToParcel(parcelW, 0);
