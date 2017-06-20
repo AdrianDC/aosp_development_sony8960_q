93d722b66523074fbcf66813ab02d7a0f4401ffb
Michael Plass <mplass@google.com>
2017-06-20 08:33:28 -0700

    Fix wifi coverage.sh
    Bug: 38318214
    Test: cd frameworks/opt/net/wifi/service/java/com/android/server/wifi
    Test: ./coverage.sh
    
    Change-Id: I9942d73ca9b79bea904fdf8369dab514664aa88d

--- tests/wifitests/coverage.sh
+++ tests/wifitests/coverage.sh
@@ -44,7 +44,7 @@ adb shell rm -f $REMOTE_COVERAGE_OUTPUT_FILE
 
 adb install -r -g "$OUT/data/app/FrameworksWifiTests/FrameworksWifiTests.apk"
 
-adb shell am instrument -e coverage true -w 'com.android.server.wifi.test/android.support.test.runner.AndroidJUnitRunner'
+adb shell am instrument -e coverage true -w 'com.android.server.wifi.test/com.android.server.wifi.CustomTestRunner'
 
 mkdir -p $OUTPUT_DIR
 
