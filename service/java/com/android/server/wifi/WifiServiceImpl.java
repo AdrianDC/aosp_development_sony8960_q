a9d209ad7cb7e5e4f9374e29aacfc49bbe304632
Roshan Pius <rpius@google.com>
2017-09-27 14:30:39 -0700

    WifiService: Enforce permission for enableVerboseLogging
    Using enforce instead of silently ignoring calls with insufficient
    permissions to let apps know that the operation failed.
    
    Bug: 67008628
    Test: Unit test
    Test: Toggle verbose logging successfully from Settings.
    Test: Regression tests (b/67380301) and ensured that tests can still
    toggle verbose logging.
    Change-Id: I15f3de0cfb8cbc68862aa30434c561d3f06a3e4b

--- service/java/com/android/server/wifi/WifiServiceImpl.java
+++ service/java/com/android/server/wifi/WifiServiceImpl.java
@@ -2383,6 +2383,7 @@ public class WifiServiceImpl extends IWifiManager.Stub {
     @Override
     public void enableVerboseLogging(int verbose) {
         enforceAccessPermission();
+        enforceNetworkSettingsPermission();
         mLog.info("enableVerboseLogging uid=% verbose=%")
                 .c(Binder.getCallingUid())
                 .c(verbose).flush();
