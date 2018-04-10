1bf8b174038c026e51ccc624825a49f5a29022e0
Love Khanna <love.khanna@nxp.com>
2018-04-10 21:58:24 +0530

    NCI2.0 Support for System Code Based Routing registration and Power State Handling.
    Register the default system code for felica offhost CE.
    NCI2.0 power states for protocol and technology routing
    
    Bug: 69876039
    Test: Perform Felica CE with System code FE0F;
    NCI ACTION NTF for SCBR expected with route to eSE;
    In NCI2.0 mode, Perform CE offhost in screen on lock, off lock and screen off unlock;
    
    Change-Id: I728874d09088d4c559772c19c423837c13cd17a0
    (cherry picked from commit 5da49837426f20a65b385d0c58f3320f4126026a)

--- nci/jni/RoutingManager.h
+++ nci/jni/RoutingManager.h
@@ -23,8 +23,11 @@
 #include "RouteDataSet.h"
 #include "SyncEvent.h"
 
+#include <map>
 #include "nfa_api.h"
 #include "nfa_ee_api.h"
+
+using namespace std;
 
 class RoutingManager {
  public:
@@ -73,6 +76,7 @@ class RoutingManager {
   static int com_android_nfc_cardemulation_doGetAidMatchingMode(JNIEnv* e);
 
   std::vector<uint8_t> mRxDataBuffer;
+  map<int, uint16_t> mMapScbrHandle;
 
   // Fields below are final after initialize()
   nfc_jni_native_data* mNativeData;
@@ -81,7 +85,12 @@ class RoutingManager {
   int mDefaultEe;
   int mAidMatchingMode;
   int mNfcFOnDhHandle;
+  bool mIsScbrSupported;
+  uint16_t mDefaultSysCode;
+  uint16_t mDefaultSysCodeRoute;
+  uint8_t mDefaultSysCodePowerstate;
   bool mReceivedEeInfo;
+  tNFA_EE_CBACK_DATA mCbEventData;
   tNFA_EE_DISCOVER_REQ mEeInfo;
   tNFA_TECHNOLOGY_MASK mSeTechMask;
   static const JNINativeMethod sMethods[];
