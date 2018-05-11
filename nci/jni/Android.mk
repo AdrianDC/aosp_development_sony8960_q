3f38a090e05d2e7fba4363d01ef01fe33779c5f1
George Chang <georgekgchang@google.com>
2018-05-11 20:51:26 +0800

    Enable debug logs for libnfc_nci_jni
    Test: Boots, NFC Enable/Disable, enable debug logs
    Bug: 79666529
    Change-Id: I27cceea9ce5096300af6541e6218478509815d49

--- nci/jni/Android.mk
+++ nci/jni/Android.mk
@@ -12,6 +12,10 @@ endif
 
 LOCAL_CFLAGS += -Wall -Wextra -Wno-unused-parameter -Werror
 
+ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
+LOCAL_CFLAGS += -DDCHECK_ALWAYS_ON
+endif
+
 LOCAL_SRC_FILES := $(call all-subdir-cpp-files) $(call all-subdir-c-files)
 
 LOCAL_C_INCLUDES += \
