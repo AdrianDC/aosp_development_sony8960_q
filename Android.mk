f0a68398b3eb424eca794b8c3828cc27a5d0c79d
Adrian DC <radian.dc@gmail.com>
2018-08-10 20:39:15 +0200

    display: Support AOSP build environment
    Change-Id: I05dab5f0f92522302bf1fa0fa6bf078fd52cfeab

--- Android.mk
+++ Android.mk
@@ -1,5 +1,3 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-display))
-
 display-hals := libgralloc libgenlock libcopybit libvirtual
 display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
 display-hals += libmemtrack
@@ -14,6 +12,4 @@ ifneq ($(filter msm8226 msm8x26 msm8960 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)
     #This is for mako since it doesn't have the QCOM make functions
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
-endif
-
 endif
