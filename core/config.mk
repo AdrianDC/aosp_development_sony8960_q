96c864ede8139040b86a812cbff6d8534c3c92ec
Adrian DC <radian.dc@gmail.com>
2019-08-10 21:53:10 +0200

    Revert "config.mk: Import LineageOS qcom_utils.mk"
    This reverts commit fae7f71a5b6932badfaa4b5b955a44613d9c7d16.

--- core/config.mk
+++ core/config.mk
@@ -1186,7 +1186,4 @@ endif
 DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
-# Rules for QCOM targets
--include $(BUILD_SYSTEM)/qcom_utils.mk
-
 include $(BUILD_SYSTEM)/dumpvar.mk
